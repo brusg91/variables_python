350b432e8827cb8a813edfacd24513e17bc322a8
brusg91 <101155426+brusg91@users.noreply.github.com>
2022-03-19 13:07:06 -0300

    Add files via upload

---
+++ ejemplos_clase/ejercicio_4.py
@@ -0,0 +1,37 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+# Si desea puede modificar el código para ingresar más palabras
+print('Ingrese palabra 1:')
+palabra_1 = str(input())
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input())
+
+print('Ingrese palabra 3:')
+palabra_3 = str(input())
+
+# De cada palabra debe tomar la primera letra y armar el acrónimo
+# Ejemplo: Alumbrado, barrido y limpieza --> ABL
+# Imprimir el resultado en pantalla
+
+
+#Ejercicios Practica 4
+#Autor: Bruno San Giorgio
+
+primer_letra_palabra_1 = palabra_1[0]   #Genero la variable de la primer letra palabra 1
+
+primer_letra_palabra_2 = palabra_2[0]   #Genero la variable de la primer letra palabra 2
+
+primer_letra_palabra_3 = palabra_3[0]   #Genero la variable de la primer letra palabra 3
+
+print('--> ',primer_letra_palabra_1+primer_letra_palabra_2+primer_letra_palabra_3)  #Imprimo el Acronimo como en el Ejemplo
