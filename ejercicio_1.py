b9529284944e1dc5065c25911d48ef2b5a78ec74
brusg91 <101155426+brusg91@users.noreply.github.com>
2022-03-19 13:05:31 -0300

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,41 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+from cgi import print_directory
+
+
+numero_1 = 5
+numero_2 = 7
+
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+# Almacenar el valor de la suma en una variable
+# ej:
+# operacion = .....
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+
+# Repita el procedimiento para realizar la resta
+
+
+#Ejercicios Practica 1
+#Autor: Bruno San Giorgio
+
+suma = numero_1 + numero_2  #Opero la suma 
+
+print('El resultado de la suma es',suma)    #Imprimo en consola el resultado metodo 1
+print('El resultado de sumar', numero_1,'y', numero_2,'es',suma)    #Imprimo en consola el resultado metodo 2
+
+resta = numero_1 - numero_2 #Opero la resta 
+
+print('El resultado de la resta es',resta) #Imprimo en consola el resultado metodo 1
+print('El resultado de restar',numero_1,'y',numero_2,'es',resta)    #Imprimo en consola el resultado metodo 2
